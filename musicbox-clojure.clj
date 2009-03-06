0ec3279fefed667ec3aa48b6147098b8bf7044d6
Andrew Stein <texodusmedia@gmail.com>
2009-03-06 14:23:24 -0500

    Cleaned up code, added --> macro

--- musicbox-clojure.clj
+++ musicbox-clojure.clj
@@ -28,7 +28,7 @@
   (:import [javax.sound.midi MidiSystem Sequencer Sequence Synthesizer]
 	   [org.jfugue MidiRenderer MusicStringParser Pattern Player Rhythm]
 	   [java.io File BufferedReader InputStreamReader])
-  (:use [clojure.contrib.str-utils :only (str-join)])
+  (:use [clojure.contrib.str-utils :only (str-join)]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;
@@ -43,9 +43,7 @@
 
 (defn xor 
   [& args]
-  (-> (filter identity args) 
-      count 
-      odd?))
+  (-> (filter identity args) count odd?))
 
 (defn pause 
   [] 
@@ -57,6 +55,12 @@
     (conj all  one)
     all))
 
+(defmacro -->
+  ([x form] (if (seq? form)
+              `(~@form ~x)
+              (list form x)))
+  ([x form & more] `(--> (--> ~x ~form) ~@more)))
+
 (defn pair-off 
   [x y]
   (partition 2 (interleave x y)))
@@ -64,21 +68,16 @@
 (defn- voice-length 
   "Determine the total duration of a vector of notes"
   [notes]
-  (reduce + 
-	  (map :duration 
-	       notes)))
+  (reduce + (map :duration notes)))
 
 (defn- resize-voice 
   "Modulate a list of notes to be a specific (longer) total duration"
   [length voice] 
   (let [head (first voice) 
 	tail (rest (cycle voice))]
-    (if (< (head :duration) 
-	   length)
+    (if (< (head :duration) length)
       (cons head 
-	    (resize-voice (- length 
-			     (head :duration)) 
-			  tail))
+	    (resize-voice (- length (head :duration)) tail))
       (vector (struct note 
 		      length 
 		      (head :pitch))))))
@@ -88,15 +87,11 @@
   ([voices] 
      (longest nil voices))
   ([top voices] 
-     (if (= (count voices) 
-	    0)
+     (if (= (count voices) 0)
        top
-       (if (< (voice-length top) 
-	      (voice-length (first voices)))
-	 (longest (first voices) 
-		  (rest voices))
-	 (longest top 
-		  (rest voices))))))
+       (if (< (voice-length top) (voice-length (first voices)))
+	 (longest (first voices) (rest voices))
+	 (longest top (rest voices))))))
 
 (defn- splice 
   "Join together a sequence of meters"
@@ -137,8 +132,7 @@
 	    longest 
 	    voice-length 
 	    (resize-voice %))  
-       (filter identity 
-	       voices)))
+       (filter identity voices)))
 
 ; Determines what a new voice looks like - this is the first conversion 
 ; from grammar to concrete note
@@ -148,12 +142,9 @@
   [parent-key new-key new-harmony]
   [(struct note 
 	   new-key 
-	   (-> (filter second 
-		       (pair-off (iterate inc 0) 
-				 new-harmony)) 
+	   (-> (filter second (pair-off (iterate inc 0) new-harmony)) 
 	       cycle 
-	       (nth (+ new-key 
-		       parent-key)) 
+	       (nth (+ new-key parent-key)) 
 	       first))])
 
 ; The main composition function walks the tree and calls the harmonize and 
@@ -161,29 +152,18 @@
 
 (defn compose 
   "Compose a grammar tree into a vector voices"
-  ([grammar] 
-     (compose (count (grammar :rhyme)) 
-	      (apply vector 
-		     (take 12 
-			   (repeat false))) 
-	      grammar))
+  ([grammar] (compose (count (grammar :rhyme)) 
+		      (apply vector (take 12 (repeat false))) 
+		      grammar))
   ([inherited-key inherited-harmony {:keys [children rhyme harmony instrument]}]
-     (splice (for [current-key (take inherited-key 
-				     (cycle rhyme))]
-	       (let [current-harmony (harmonize-down inherited-harmony 
-						     harmony) 
-		     current-rhyme (synchronize-down inherited-key 
-						     rhyme)
-		     voices (apply concat 
-				   (map #(compose current-key 
-						  current-harmony 
-						  %) 
-					children))]
+     (splice (for [current-key (take inherited-key (cycle rhyme))]
+	       (let [current-harmony (harmonize-down inherited-harmony harmony) 
+		     current-rhyme (synchronize-down inherited-key rhyme)
+		     voices (--> children
+				 (map #(compose current-key current-harmony %))
+				 (apply concat))]
 		 (-> (if instrument
-		       (conj voices 
-			     (new-voice current-key 
-					current-rhyme 
-					current-harmony)) 
+		       (conj voices (new-voice current-key current-rhyme current-harmony)) 
 		       voices) 
 		     harmonize-up 
 		     synchronize-up))))))
@@ -199,12 +179,10 @@
  
 (defn- note-string 
   [note octave]
-  (let [note-str (nth note-table 
-		      (note :pitch))]
+  (let [note-str (nth note-table (note :pitch))]
     (str note-str
-	 (if (!= note-str "R") 
-	   (+ octave 2))
-	 (apply str (take (note :duration)
+	 (if (!= note-str "R") (+ octave 2))
+	 (apply str (take (note :duration) 
 			  (repeat "q"))))))
 
 (defn- voice-string 
@@ -215,15 +193,11 @@
 
 (defn- build-string 
   [voices]
-  (str-join \ 
-	    (for [voice (pair-off voices 
-				  (iterate inc 1))]
-		  (str \v 
-		       (second voice) 
-		       \ 
-		       (voice-string 
-			(first voice) 
-			(second voice))))))
+  (str-join \ (for [voice (pair-off voices (iterate inc 1))]
+		(str \v 
+		     (second voice) 
+		     \ 
+		     (voice-string (first voice) (second voice))))))
 
 (defn- build-midi 
   [string]
@@ -239,16 +213,10 @@
 
 (defn- play-midi 
   [midi]
-  (with-open [synth (doto (. MidiSystem 
-			     getSynthesizer) 
-		      (.open))
-              seqr (doto (. MidiSystem 
-			    (getSequencer false)) 
-		     (.open))]
-      (do (.setReceiver (.getTransmitter seqr) 
-			(.getReceiver synth))
-          (.setSequence seqr 
-			midi)
+  (with-open [synth (doto (. MidiSystem getSynthesizer) (.open))
+              seqr (doto (. MidiSystem (getSequencer false)) (.open))]
+      (do (.setReceiver (.getTransmitter seqr) (.getReceiver synth))
+          (.setSequence seqr midi)
           (.start seqr)
           (pause))))
 
@@ -291,6 +259,7 @@
 			 true
 			 [])]))
 )
+
 
 
 
