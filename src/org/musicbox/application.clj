0ecc5eecf45203774d73b74d3d357a11ee70a96f
Andrew <astein@mojiva.com>
2009-03-11 16:13:20 -0400

    Created ant script;  fixed some UI bugs

--- src/org/musicbox/application.clj
+++ src/org/musicbox/application.clj
@@ -24,24 +24,42 @@
 ; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 ; OTHER DEALINGS IN THE SOFTWARE. 
 
-(ns musicbox
+(ns org.musicbox.application
+  (:gen-class)
   (:import [javax.sound.midi MidiSystem Sequencer Sequence Synthesizer]
            [java.io File BufferedReader InputStreamReader]
            [java.awt.event ActionListener]
+           [javax.swing.event ChangeListener]
            [java.util Random]
-           [javax.swing JPanel JFrame JLabel JButton JSlider]
+           [javax.swing JPanel JFrame JLabel JButton JSlider JProgressBar UIManager JRootPane]
+	   [org.musicbox GUI]
            [org.jfugue MidiRenderer MusicStringParser Pattern Player Rhythm])
   (:use [clojure.contrib.str-utils :only (str-join)]))
-
-
-(defstruct note :duration :pitch :velocity)
-(defstruct grammar :key-seq :rhyme :harmony :velocity :instrument :children)
-(defstruct voice :instrument :notes)
-(defstruct setting :min :max :default :current)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;
 ;;;; Common
+
+;;;; Constants 
+
+(def note-table ["A" "A#" "B" "C"
+                 "C#" "D" "D#" "E"
+                 "F" "F#" "G" "G#"])
+
+(def bass-instrument-table ["Rock_Organ" "Tubular_Bells"
+                            "Electric_Bass_Pick" "Synth_Voice"
+                            "Piano" "Woodblock"])
+
+(def lead-instrument-table ["Violin" "Piano" 
+                            "Alto_Sax" "Synthbrass_1"
+                            "Flute" "Sawtooth"
+                            "Orchestral_Strings"])
+
+(defstruct note :duration :pitch :velocity)
+(defstruct grammar :key-seq :rhyme :harmony :velocity :instrument :children)
+(defstruct voice :instrument :notes)
+
+;;;; Utility
 
 (defn != 
   [a b] 
@@ -55,6 +73,8 @@
   [x y]
   (partition 2 (interleave x y)))
 
+;;;; These are utility functions for mising voices
+
 (defn- voice-length 
   "Determine the total duration of a vector of notes"
   [voice]
@@ -66,12 +86,14 @@
   "Modulate a list of notes to be a specific (longer) total duration"
   [voice length] 
   (assoc voice 
-    :notes (let [head (first (voice :notes)) 
-                 tail (assoc voice 
-                        :notes (rest (cycle (voice :notes))))]
-             (if (< (head :duration) length)
-               (cons head ((resize-voice tail (- length (head :duration))) :notes))
-               (vector (struct note length (head :pitch) (head :velocity)))))))
+    :notes (let [notes (voice :notes)
+                 head (first notes) 
+                 tail (take (count notes) (rest (cycle notes)))]
+             (if (or (= (count notes) 1)  
+                     (>= (head :duration) length))
+               (vector (assoc head :duration length))               
+               (cons head (:notes (resize-voice (assoc voice :notes tail) 
+                                                (- length (head :duration)))))))))
 
 (defn- longest
   "Determines the voice with the longest total duration"
@@ -114,13 +136,7 @@
   [voices]
   (let [max-voice (voice-length (longest voices))]
     (for [voice (filter #(-> % identity :notes empty? not) voices)]
-      (if (< (count (voice :notes)) 2)
-        (assoc voice 
-          :notes (vector (struct note 
-                                 max-voice 
-                                 ((first (voice :notes)) :pitch) 
-                                 ((first (voice :notes)) :velocity))))
-        (resize-voice voice max-voice)))))
+      (resize-voice voice max-voice))))
 
 ; The main composition function walks the tree and calls the synchronize 
 ; functions when necessary
@@ -146,19 +162,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;
 ;;;; Style
-
-(def note-table ["A" "A#" "B" "C"
-                 "C#" "D" "D#" "E"
-                 "F" "F#" "G" "G#"])
-
-(def bass-instrument-table ["Rock_Organ" "Tubular_Bells"
-                            "Orchestra_Hit" "Synth_Voice"
-                            "Synth_Bass_1" "Woodblock"])
-
-(def lead-instrument-table ["Violin" "Piano" 
-                            "Alto_Sax" "Synthbrass_1"
-                            "Flute" "Sawtooth"
-                            "Orchestral_Strings"])
 		 
 (defn generate-grammar
   "Build a semi-random grammar"
@@ -176,7 +179,7 @@
                     (iterate (fn [_] (rnd 4 1))
                              (rnd 4 1))))
      :harmony (take (rnd 4 2)
-                    (iterate (fn [_] (if (> (rnd 6 0) 2) 
+                    (iterate (fn [_] (if (> (rnd 6 0) 1) 
                                        (nth note-table (rnd 12 0))
                                        "R"))
                              (nth note-table (rnd 12 0))))
@@ -191,14 +194,12 @@
      :children (if (> depth 0)
                  (vector (generate-grammar (dec depth)))
                  (vector))}))
-
-
 			   
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;
 ;;;; MIDI
 
-; These functions build a JFugue MusicString from note vectors
+;;;; These functions build a JFugue MusicString from note vectors
  
 (defn- note-string 
   [note octave]
@@ -220,17 +221,20 @@
   (str-join \ (for [label-voice (pair-off voices (iterate inc 1))]
                 (str \v (second label-voice) 
                      \ "Rww" 
-                     \ (voice-string (first label-voice) (second label-voice))))))
+                     \ (voice-string (first label-voice) (second label-voice))
+                     \ "Rww"))))
 
-;  MIDI and JFugue utility functions
-(def player-run-flag
-     (atom false))
+;;;; MIDI and JFugue utility functions
+
+(def player-run-flag (atom false))
+(def progress (atom 0))
+(def tempo (atom 1))
 
 (defn- build-midi 
   [string]
   (let [pattern (Pattern. string)
         parser (MusicStringParser.)
-        renderer (MidiRenderer. 0 600)]
+        renderer (MidiRenderer. 0 200)]
     (do
       (. System/out println string)
       (. parser (addParserListener renderer))
@@ -243,10 +247,12 @@
               seqr (doto (. MidiSystem (getSequencer false)) (.open))]
       (do (.setReceiver (.getTransmitter seqr) (.getReceiver synth))
           (.setSequence seqr midi)
-          (reset! player-run-flag true)
           (.start seqr)
           (while (and @player-run-flag (.isRunning seqr)) 
-            (. Thread (sleep 500)))
+            (. Thread (sleep 500))
+            (reset! progress (quot (* 100 (. seqr getTickPosition)) 
+                                   (. seqr getTickLength)))
+            (. seqr (setTempoFactor @tempo)))
           (.println System/out "Finished"))))
 
 (defn play 
@@ -261,32 +267,54 @@
 ;;;;
 ;;;; GUI
 
+(def panel (GUI.))
+
+(def player-agent (agent nil))
+
+(def progress-agent (send-off (agent nil) 
+                              (fn [_] (while true 
+                                (do (.. panel getJProgressBar (setValue @progress))
+                                    (. Thread (sleep 500)))))))
+
 (defn play-song 
   [_]
-  (play (generate-grammar 3)))
+  (do
+    (reset! player-run-flag true)
+    (while @player-run-flag
+      (play (let [main-grammar (generate-grammar 2)]
+              (struct grammar
+                      [1 1 2 1]
+                      [1]
+                      []
+                      []
+                      false
+                      [main-grammar]))))
+    (reset! progress 0)))
 
-(def player-agent
-     (agent nil))
 
-(def panel 
-     (doto (JPanel.)
-       (.add (doto (JButton. "Reset")
-               (.addActionListener (proxy [ActionListener] []
-                                     (actionPerformed [actionEvent]
-                                                      (do (reset! player-run-flag false)
-                                                          (send-off player-agent play-song)))))))
-       (.add (JSlider.))))           
-
-(def frame 
-     (doto (JFrame.)
-       (.setTitle "Clojure Music Box")
-       (.setDefaultCloseOperation (. JFrame EXIT_ON_CLOSE))
-       (.add panel)
-       (.setResizable false)
-       .pack 
-       .show))
-
-;;;; emacs
-(comment
-  (load-file "musicbox-clojure.clj")
-)
+(defn -main [& args] 
+  (do
+    ; Create the application frame
+    (def frame 
+	 (doto (JFrame.)
+	   (.setTitle "Clojure Music Box")
+	   (.setDefaultCloseOperation (. JFrame EXIT_ON_CLOSE))
+	   (.add panel)
+	   (.setResizable false)
+	   .pack 
+	   .show))
+    ; Add ActionListeners for the various controls
+    (. (. panel getJButton1) 
+       (addActionListener (proxy [ActionListener] []
+                            (actionPerformed [event]
+                                             (do (reset! player-run-flag false)
+                                                 (send-off player-agent play-song))))))
+    (. (. panel getJButton2)
+       (addActionListener (proxy [ActionListener] []
+                            (actionPerformed [event]
+                                             (do (reset! player-run-flag false))))))       
+    (. (. panel getJSlider)
+       (addChangeListener (proxy [ChangeListener] []
+                            (stateChanged [event]
+                                          (do (reset! tempo (+ 0.25 (* 1.5 (/ (.. event getSource getValue) 100)))))))))      
+    (. UIManager (setLookAndFeel (. UIManager getSystemLookAndFeelClassName)))))
